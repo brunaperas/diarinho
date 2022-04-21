396b8c777b3a7c33fb525d42e6d8c95308f48b71
Bruna Pereira <perasbruna@gmail.com>
2022-04-21 01:50:22 -0300

    controles-estilo

--- js/script.js
+++ js/script.js
@@ -8,20 +8,35 @@ class editaTexto {
     this.personaliza = document.querySelector('.personaliza');
     this.estiloTexto = document.querySelector('.estilo-texto'); 
     
-    this.textoFinal = document.createElement('div');
-
+    this.tituloFinal = document.createElement ('h1');
+    this.tituloFinal.classList.add('titulo-geral');
+    this.textoFinal = document.createElement('p');
+    this.textoFinal.classList.add('texto-geral');
+    this.dataFinal = document.createElement('span'); 
+    this.dataFinal.classList.add('data-geral');
+    this.corpoFinal = document.createElement('section');
+    this.corpoFinal.classList.add('estilo-geral');
+    
     this.corpoTexto = this.corpoTexto.bind(this);
+    this.changeEstilo = this.changeEstilo.bind(this);
   }
 
 
   corpoTexto() {
     this.titulo = this.caixaTitulo.value;
     this.texto = this.caixaTexto.value;
-    this.data = this.caixaData.value;
-    this.saveValues()
+    this.data = this.caixaData.value.split('-').reverse().join('\/');
+
+    this.saveValues();
     if (this.titulo && this.texto && this.data) {
-      this.textoFinal.innerHTML = this.titulo + ' ' + this.data + '</br>' + this.texto;  
-      this.personaliza.append(this.textoFinal);
+      this.tituloFinal.innerHTML = this.titulo;
+      this.textoFinal.innerText = this.texto;
+      this.dataFinal.innerHTML = this.data;
+
+      this.personaliza.append(this.corpoFinal);
+      this.corpoFinal.appendChild(this.tituloFinal);
+      this.corpoFinal.appendChild(this.dataFinal);
+      this.corpoFinal.appendChild(this.textoFinal);
     }
   }
   insereTexto() {
@@ -37,24 +52,59 @@ class editaTexto {
     this.caixaTitulo.value = localStorage.getItem('titulo', this.titulo);
     this.caixaTexto.value = localStorage.getItem('texto', this.texto);
     this.caixaData.value = localStorage.getItem('data', this.data);
+
+    this.tituloFinal.innerHTML = localStorage.getItem('titulo', this.titulo);
+    this.textoFinal.innerText = localStorage.getItem('texto', this.texto);
+    this.dataFinal.innerHTML = localStorage.getItem('data', this.data);
+
+    this.personaliza.append(this.corpoFinal);
+    this.corpoFinal.appendChild(this.tituloFinal);
+    this.corpoFinal.appendChild(this.dataFinal);
+    this.corpoFinal.appendChild(this.textoFinal);
   }
+
+ changeEstilo() {
+   this.fonteTitulo = this.caixaFonteTitulo.value;
+  this.corTitulo = this.caixaCorTitulo.value;
+  this.tamanhoTitulo = this.caixaTamanhoTitulo.value;
+
+  this.fonteTexto = this.caixaFonteTexto.value;
+  this.corTexto = this.caixaCorTexto.value;
+  this.tamanhoTexto = this.caixaTamanhoTexto.value;
+
+  this.tituloFinal.style.fontFamily = this.fonteTitulo;
+  this.tituloFinal.style.color = this.corTitulo;
+  this.tituloFinal.style.fontSize = this.tamanhoTitulo + 'px';
+
+  this.textoFinal.style.fontFamily = this.fonteTexto;
+  this.textoFinal.style.color = this.corTexto;
+  this.textoFinal.style.fontSize = this.tamanhoTexto + 'px';
+  } 
+
+  estiloFontes() {
+    this.controles = document.querySelector('.controles'); 
+
+    this.caixaFonteTitulo = document.querySelector('#fonteTitulo');
+    this.caixaCorTitulo = document.querySelector('#colorTitulo');
+    this.caixaTamanhoTitulo = document.querySelector('#tamanhoTitulo');
+
+    this.caixaFonteTexto = document.querySelector('#fonteTexto');
+    this.caixaCorTexto = document.querySelector('#colorTexto');
+    this.caixaTamanhoTexto = document.querySelector('#tamanhoTexto');
+
+    this.controles.addEventListener('change', this.changeEstilo);
+  }
+
   init() {
     this.setValues();
-    this.insereTexto();   
+    this.insereTexto();  
+    this.estiloFontes(); 
     return this;
   }
 }
 
 const Edita = new editaTexto();
 Edita.init();
-
-
-
-
-
-
-
-
 
 
 
